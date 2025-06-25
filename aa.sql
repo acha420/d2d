0a4ae1e73e537cec983b58729ecda48527c70a8f
acha420 <patelachal42@gmail.com>
2025-06-25 15:39:26 +0530

    Add files via upload

---
+++ aa.sql
@@ -0,0 +1,14 @@
+-- create database salonproject;
+use  d2dc;
+-- create table  student(id int,st_name text,age int );
+
+-- insert into student(id,st_name,age) values(1,'achal',18);
+
+-- select  * from student;
+-- insert into student(id,st_name,age)values (1,'bhavya',20),(2,'rushik',23),(3,'deepali',25);
+-- insert into  student(id,st_name) values(4,'ved'); 
+insert into  student(id,st_name) values(5,'hetav'); 
+
+
+
+
