f2ac1a28303396e6f6a777dcb85946675470b7fe
acha420 <patelachal42@gmail.com>
2025-06-25 16:00:33 +0530

    Add files via upload

--- aa.sql
+++ aa.sql
@@ -7,7 +7,23 @@ use  d2dc;
 -- select  * from student;
 -- insert into student(id,st_name,age)values (1,'bhavya',20),(2,'rushik',23),(3,'deepali',25);
 -- insert into  student(id,st_name) values(4,'ved'); 
-insert into  student(id,st_name) values(5,'hetav'); 
+-- insert into  student(id,st_name) values(5,'hetav'); 
+
+-- update student  set st_name='arya' where id=1;
+-- update student set st_name='bhavya' where age=20;
+-- update student set id=2 where age=20;
+-- update student set id=3 where age=23;
+-- update student set id=4 where age=25;
+-- update student set id=5 where st_name ='ved';
+-- update student set id=4 where st_name ='deepali';
+-- update student set id=6 where st_name ='hetav';
+
+delete from student where age=20;
+delete from student where st_name='hetav';
+
+
+
+
 
 
 
