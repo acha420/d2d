96592abe76ad5625591c9ee2c461aa09c8940d71
acha420 <patelachal42@gmail.com>
2025-07-02 15:59:35 +0530

    Add files via upload

---
+++ sql2.sql
@@ -0,0 +1,32 @@
+-- create database clinic;
+use clinic;
+
+
+-- create table  paitents(
+-- id int,
+-- p_name varchar(50),
+-- co_number int,
+-- visiting_time datetime,
+-- address varchar(200)
+-- );
+
+-- insert into paitents(id,p_name,co_number,visiting_time,address) values(1,'achal','63521','2000-01-01 12:12:12','vadodara');
+-- insert into paitents(id,p_name,co_number,visiting_time,address) values(1,'rushik','6352','2000-01-02 12:12:12','ahemdabad');
+-- insert into paitents(id,p_name,co_number,visiting_time,address) values(1,'bhavya','6352','2000-01-03 12:12:12','gandhinagar');
+-- select *from paitents where address='vadodara';
+-- select *from paitents where address!='vadodara';
+ -- select *from paitents where address='vadodara' and visiting_time>'2000-01-01';
+      --    select *from paitents where p_name like 'a%';
+  --    select *from paitents where p_name like '%a';
+  -- select *from paitents where p_name like '%y%';
+-- select *from paitents where p_name like '_____a';
+
+
+
+-- select *from paitents order by visiting_time desc;
+
+-- select *from paitents limit 2;
+
+-- select  *from paitents  order by  visiting_time  limit 2;
+
+
