34c1fc46726cade81ec161bf3ca2ec5ad7a80cc0
acha420 <patelachal42@gmail.com>
2025-07-10 16:15:43 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,14 @@
+public class Laptop{
+    String brand;
+    int ram;
+    boolean istuchscreen;
+
+    public static void main(String[] args){
+        Laptop mylaptop=new Laptop();
+
+        System.out.println("brand"+mylaptop.brand);
+                System.out.println("ram"+mylaptop.ram);
+        System.out.println("brand"+mylaptop.istuchscreen);
+
+    }
+}
