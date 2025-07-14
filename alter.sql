37b550ad2213dc2b50880cc5545c1c3faab1e4cf
acha420 <patelachal42@gmail.com>
2025-07-14 11:22:41 +0530

    Add files via upload

---
+++ alter.sql
@@ -0,0 +1,13 @@
+use d2dc;
+-- create table ncc(id int primary key auto_increment,
+-- camp_name varchar(50),
+-- no_ofcudets int
+-- );
+
+-- alter table ncc add column no_oftank int default(0);
+
+
+-- alter table ncc drop column no_oftank;
+
+-- alter table ncc modify column no_ofcudets varchar(50);
+-- alter table ncc rename column no_ofcudets to no_ofguns;
